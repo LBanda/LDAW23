1a9a39408b0c681aa30de9d3d2c5b0af25cf9a5e
LBanda <A01610329@itesm.mx>
2023-08-29 20:41:42 -0600

    practica grupal React

---
+++ PRACTICA GRUPAL APLICACIÓN SIMPLE CON REACT/APPSimpleReact/appsimple/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import VideojuegosApp from './VideojuegosApp';
+import './index.css';
+
+const container = document.getElementById('root');
+
+
+const root = createRoot(container);
+
+//Utilizamos el JSX que devuelve el componente VideojuegosApp para injectar ese html
+// en divRoot
+
+root.render(<VideojuegosApp/>);
